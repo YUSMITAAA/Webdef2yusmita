08277b4cfe8a8097ee456dca6b12ae539bbc45e9
YUSMITAAA <100122820+YUSMITAAA@users.noreply.github.com>
2023-05-27 17:48:33 +0800

    Add files via upload

---
+++ BUKU_TAMU/BUKU_TAMU/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BUKU_TAMU project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BUKU_TAMU.settings')
+
+application = get_asgi_application()
